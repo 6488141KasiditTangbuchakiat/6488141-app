8dfc52edd01bcee3bb1ce2d7ee6707de467e74dc
Terng <142678886+6488141KasiditTangbuchakiat@users.noreply.github.com>
2024-02-05 17:15:18 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -7,10 +7,7 @@ class MyApp extends StatelessWidget {
  return MaterialApp(
  title: 'Flutter Demo',
  theme: ThemeData(
- primarySwatch: Colors.blue,
-
-
- ),
+ primarySwatch: Colors.blue,),
  home: MyHomePage(title: 'Product layout demo home page'),
  );
  }
@@ -21,52 +18,34 @@ class MyHomePage extends StatelessWidget {
  @override
  Widget build(BuildContext context) {
  return Scaffold(
- appBar: AppBar(title: Text("Product Listing")),
+ appBar: AppBar(title: Text("Saved Recipe")),
  body: ListView(
  shrinkWrap: true,
- padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+ padding: const EdgeInsets.fromLTRB(5, 10.0, 2.0, 10.0),
  children: <Widget>[
  ProductBox(
- name: "iPhone",
- description: "iPhone is the stylist phone ever",
- price: 1000,
- image: "iphone.jpg"),
+ name: "recipe",
+ description: "recipe",
+ 
+ image: "img.png"),
  ProductBox(
- name: "Pixel",
- description: "Pixel is the most featureful phone ever",
- price: 800,
- image: "pixel.jpg"),
- ProductBox(
- name: "Laptop",
- description: "Laptop is most productive development tool",
- price: 2000,
- image: "laptop.jpg"),
- ProductBox(
- name: "Tablet",
- description: "Tablet is the most useful device ever for meeting",
- price: 1500,
- image: "tablet.jpg"),
- ProductBox(
- name: "Pendrive",
- description: "Pendrive is useful storage medium",
- price: 100,
- image: "pendrive.jpg"),
- ProductBox(
- name: "Floppy Drive",
- description: "Floppy drive is useful rescue storage medium",
- price: 20,
- image: "floppydisk.jpg"),
+ name: "recipe",
+ description: "recipe",
+ 
+ image: "img.png"),
+
+
  ],
  ));
  }
 }
 
 class ProductBox extends StatelessWidget {
- ProductBox({Key? key, required this.name, required this.description, required this.price, required this.image})
+ ProductBox({Key? key,required this.name, required this.description,required this.image})
  : super(key: key);
  final String name;
  final String description;
- final int price;
+
  final String image;
  Widget build(BuildContext context) {
  return Container(
@@ -87,7 +66,7 @@ child: Column(
  style: TextStyle(fontWeight:
 FontWeight.bold)),
  Text(this.description),
- Text("Price: " + this.price.toString()),
+ 
  ],
  )))
  ])));
